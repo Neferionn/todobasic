17602d42d5be47cccc2a4344f7d9d1ab4faa0a13
Neferionn <anonymperson@mail.ru>
2021-09-27 18:57:07 +0300

    refactored and added save to local storage

--- script.js
+++ script.js
@@ -1,72 +1,121 @@
-const listItemContainer = document.querySelector(".items");
-const inputValue = document.querySelector(".newItemInput");
+const input = document.querySelector('.newItemInput');
+const btnSubmit = document.querySelector('.btnSubmit');
+const itemsList = document.querySelector('.itemsList');
 
-const btnSubmit = document.querySelector(".submit");
-const btnDelete = document.querySelector(".delete");
+const addItem = e => {
+  e.preventDefault();
 
-const itemFns = e => {
-  const li = document.createElement("li");
-  const title = document.createElement("div");
-  const editInput = document.createElement("input");
-  const btns = document.createElement("div");
-  const btnEdit = document.createElement("button");
-  const btnSave = document.createElement("button");
-  const btnDelete = document.createElement("button");
+  const itemContainer = document.createElement('div');
+  itemContainer.classList.add('item');
 
-  li.classList.add("flexItem");
-  title.classList.add("itemText");
-  title.innerText = e;
+  const newItem = document.createElement('li');
+  newItem.innerText = input.value;
+  newItem.classList.add('newItem');
+  itemContainer.appendChild(newItem);
 
-  editInput.classList.add("editInput");
-  editInput.classList.add("hidden");
+  localSave(input.value);
 
-  btns.classList.add("btnsOpt");
+  const btnMark = document.createElement('button');
+  btnMark.innerHTML = '✓';
+  btnMark.classList.add('btnMark');
+  itemContainer.appendChild(btnMark);
 
-  btnEdit.innerHTML = "Edit";
-  btnEdit.classList.add("btnEdit");
+  const btnDel = document.createElement('button');
+  btnDel.innerHTML = 'X';
+  btnDel.classList.add('btnDel');
+  itemContainer.appendChild(btnDel);
 
-  btnSave.innerHTML = "Save";
-  btnSave.classList.add("hidden");
-  btnSave.classList.add("btnSave");
+  itemsList.appendChild(itemContainer);
 
-  btnDelete.innerHTML = "X";
-  btnDelete.classList.add("btnDelete");
+  input.value = '';
+  btnSubmit.disabled = true;
+  btnSubmit.classList.add('disabledBtn');
+};
 
-  li.appendChild(title);
-  li.appendChild(btns);
-  li.appendChild(editInput);
-  btns.appendChild(btnDelete);
-  btns.appendChild(btnEdit);
-  btns.appendChild(btnSave);
+const deleteCheck = e => {
+  const element = e.target;
+  if (element.classList[0] === 'btnDel') {
+    const parent = element.parentElement;
+    parent.classList.add('disappear');
+    removeLocaltodoItems(parent);
+    parent.addEventListener('transitionend', () => {
+      parent.remove();
+    });
+  }
 
-  listItemContainer.appendChild(li);
+  if (element.classList[0] === 'btnMark') {
+    const parent = element.parentElement;
 
-  const toggle = () => {
-    li.classList.toggle("flexReverse");
-    title.classList.toggle("hidden");
-    editInput.classList.toggle("hidden");
-    btnEdit.classList.toggle("hidden");
-    btnSave.classList.toggle("hidden");
-  };
+    const liChild = parent.firstChild;
 
-  // btns listeners
+    liChild.classList.toggle('completed');
+  }
+};
 
-  btnEdit.addEventListener("click", () => {
-    toggle();
-    editInput.focus();
-  });
-  btnSave.addEventListener("click", () => {
-    title.innerText = editInput.value;
-    toggle();
-  });
-  btnDelete.addEventListener("click", () => {
-    listItemContainer.removeChild(li);
+const localSave = todo => {
+  let todoItems;
+
+  if (localStorage.getItem('todoItems') === null) {
+    todoItems = [];
+  } else {
+    todoItems = JSON.parse(localStorage.getItem('todoItems'));
+  }
+  todoItems.push(todo);
+
+  localStorage.setItem('todoItems', JSON.stringify(todoItems));
+};
+
+const getTodo = () => {
+  let todoItems;
+  if (localStorage.getItem('todoItems') === null) {
+    todoItems = [];
+  } else {
+    todoItems = JSON.parse(localStorage.getItem('todoItems'));
+  }
+  todoItems.forEach(function (todo) {
+    const itemContainer = document.createElement('div');
+    itemContainer.classList.add('item');
+
+    const newItem = document.createElement('li');
+    newItem.innerText = todo;
+    newItem.classList.add('todo-item');
+
+    itemContainer.appendChild(newItem);
+
+    const btnMark = document.createElement('button');
+    btnMark.innerHTML = '✓';
+    btnMark.classList.add('btnMark');
+    itemContainer.appendChild(btnMark);
+
+    const btnDel = document.createElement('button');
+    btnDel.innerHTML = 'X';
+    btnDel.classList.add('btnDel');
+    itemContainer.appendChild(btnDel);
+
+    itemsList.appendChild(itemContainer);
   });
 };
 
-btnSubmit.addEventListener("click", function (e) {
-  e.preventDefault();
-  const taskName = inputValue.value;
-  itemFns(taskName);
-  inputValue.value = "";
-});
+const removeLocaltodoItems = todo => {
+  let todoItems;
+  if (localStorage.getItem('todoItems') === null) {
+    todoItems = [];
+  } else {
+    todoItems = JSON.parse(localStorage.getItem('todoItems'));
+  }
+  const todoIndex = todo.children[0].innerText;
+  todoItems.splice(todoItems.indexOf(todoIndex), 1);
+  localStorage.setItem('todoItems', JSON.stringify(todoItems));
+};
+
+function btnToggle() {
+  if (input.value !== '') {
+    btnSubmit.disabled = false;
+    btnSubmit.classList.remove('disabledBtn');
+  }
+}
+
+input.addEventListener('change', btnToggle);
+document.addEventListener('DOMContentLoaded', getTodo);
+btnSubmit.addEventListener('click', addItem);
+itemsList.addEventListener('click', deleteCheck);
